5c2b0ad3e06d9c652a931f96fbd7ca489b315a06
Seapolco <seapolco@gmail.com>
2022-10-26 17:38:30 +0100

    Created animation for sidebar nav icons
    Follows the same basic pattern as the source ( the nav options
    translate upwards whilst fading in downwards), needs polishing.
    Very pleased with the progress thus far.

--- src/helpers/domElements.js
+++ src/helpers/domElements.js
@@ -2,8 +2,22 @@
 const dom = function documentQuerySelectors() {
 
     const hamIcon = () => document.querySelector('.hamIcon')
+
     const sidebarContainer = () => document.querySelector('.sidebarContainer');
+    const sideBar = () => document.querySelector('.sidebar');
     const closeIconContainer = () => document.querySelector('.closeIconContainer')
+    const sidebarHeader = () => document.querySelector('.sidebarHeader')
+
+    const sidebarNavItem = () => document.querySelectorAll('.sidebarNavItem');
+
+    const shome = () => document.querySelector('.shome')
+    const sshop = () => document.querySelector('.sshop')
+    const sstream = () => document.querySelector('.sstream')
+    const sevents = () => document.querySelector('.sevents')
+    const scontact = () => document.querySelector('.scontact')
+    const sfaq = () => document.querySelector('.sfaq')
+    
+
 
     // return {
     //     'hamIcon' : hamIcon, 
@@ -14,7 +28,16 @@ const dom = function documentQuerySelectors() {
     return {
         hamIcon, 
         sidebarContainer,
-        closeIconContainer
+        closeIconContainer, 
+        sideBar,
+        sidebarHeader,
+        sidebarNavItem,
+        shome,
+        sshop,
+        sstream,
+        sevents,
+        scontact,
+        sfaq
     }
 }
 
--- src/helpers/eventListeners.js
+++ src/helpers/eventListeners.js
@@ -9,26 +9,68 @@ const eventListeners = function allEventListeners() {
     const hamIcon = domElements.hamIcon();
     const sidebarContainer = domElements.sidebarContainer();
     const closeIcon = domElements.closeIconContainer();
+    const sideBar = domElements.sideBar();
+
+    const sidebarHeader = domElements.sidebarHeader();
+    const sidebarNavItem = domElements.sidebarNavItem();
+
+    const shome = domElements.shome()
+    const sshop = domElements.sshop()
+    const sstream = domElements.sstream()
+    const sevents = domElements.sevents()
+    const scontact = domElements.scontact()
+    const sfaq = domElements.sfaq()
+
 
     sidebarContainer.style.visibility;
 
     const hamLis = () => {
          return hamIcon.addEventListener('click', (e) => {
             // console.log(visibility)
-            if (sidebarContainer.style.visibility === 'hidden') {
-                console.log('hamooooo')
-                sidebarContainer.style.visibility = 'visible';
-            } else if (sidebarContainer.style.visibility === 'visibile') {
-                sidebarContainer.style.visibility = 'hidden';
-                console.log('hamwwwwwwwwwwwwwwwww')
+            // if (sidebarContainer.style.visibility === 'hidden') {
+            //     console.log('hamooooo')
+            //     sidebarContainer.style.visibility = 'visible';
+            // } else if (sidebarContainer.style.visibility === 'visibile') {
+            //     sidebarContainer.style.visibility = 'hidden';
+            //     console.log('hamwwwwwwwwwwwwwwwww')
+            // }
+            // console.log('ham')
+            console.log(sidebarNavItem);
+
+            if(sidebarContainer.style.translate === '-100%') {
+                sidebarContainer.style.translate = '0%';
+                sideBar.style.transform = 'translateY(0%)';
+                sideBar.style.opacity = '1';
+                // shome.style.opacity = '1';
+
+                sidebarNavItem.forEach((navItem, i) => {
+                   return setTimeout(() => {
+                        navItem.style.opacity = '1';
+                    }, (i+2) * 100)
+                })
+                // sideBar.classList.remove('.sidebarHide')
+                // sideBar.classList.add('.sidebarShow')
+
+            } else {
+                sidebarContainer.style.translate = '-100%';
+                
             }
-            console.log('ham')
         })
     } 
 
     const closeLis = () => {
        return closeIcon.addEventListener('click', (e) => {
-        sidebarContainer.style.visibility = 'hidden'
+        sidebarContainer.style.translate = '-100%';
+        // sideBar.classList.remove('.sidebarShow')
+        // sideBar.classList.add('.sidebarHide')
+        setTimeout(() => {
+            sidebarNavItem.forEach((navItem) => {
+                navItem.style.opacity  = '0';
+            })
+            sideBar.style.transform = 'translateY(25%)';
+            sideBar.style.opacity = '0';
+        }, '200')
+
         })
     } 
 
